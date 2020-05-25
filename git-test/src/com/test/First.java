3b24ad61a7b29d82a0d0cb4416d0fc2844a892f5
952407502 <952407502@qq.com>
2020-05-26 02:49:48 +0800

    将项目添加到本地仓库

---
+++ git-test/src/com/test/First.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        get();
+
+    }
+    public static void get(){
+        int i=0;
+        System.out.println("i="+(++i));
+    }
+}
